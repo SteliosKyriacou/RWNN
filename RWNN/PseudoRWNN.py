fe18960186562bae728b830bf24fba8e3706391d
Unknown <stelios.kyriacou@gmail.com>
2020-05-07 09:14:07 -0700

    PseudoRWNN implementation and example

---
+++ RWNN/PseudoRWNN.py
@@ -0,0 +1,39 @@
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.constraints import Constraint
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+import numpy as np
+import tensorflow.keras.initializers as initializers
+
+
+#custom weight constraint 
+class removeSynapses (Constraint):
+    def __init__(self, size,pc,rng):
+        a,b=size
+        msk=rng.rand(a,b)
+        while ((msk>pc) == False).sum() < 1: # danger!
+            msk=rng.rand(a,b)
+        msk[msk > pc] = 0
+        msk[msk != 0] = 1
+        self.mask=tf.Variable(msk,dtype=tf.float32)
+    def __call__(self, w):
+        return tf.multiply(self.mask,w)
+    
+
+def PseudoRWNN(inputshape,layers,seed):
+    #sigmoid activation friendly weight initializations
+    tf.random.set_seed(seed)
+    init=initializers.RandomUniform(minval=-1, maxval=1, seed=seed)
+    rng=np.random.RandomState(seed)
+    #construct model using sequential 
+    model = Sequential()
+    for l in layers:
+        nodes,act,synrem = l
+        model.add(Dense(nodes, 
+                        activation=act,
+                        input_shape=(inputshape,),
+                        use_bias=False,
+                        kernel_initializer=init,
+                        kernel_constraint=removeSynapses((inputshape,nodes),synrem,rng))
+                 )
+    return model
---
+++ setup.py
@@ -0,0 +1,9 @@
+import sys
+from distutils.core import setup
+setup(name='RWNN',
+      version='v0.0',
+      author='Stylianos Kyriacou',
+      packages=['RWNN'],
+      license='MIT License (MIT License)',
+      platforms=['any']
+    )
